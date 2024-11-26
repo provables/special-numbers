ec0d22fa6ddf9fd33fd199901cdb2dcc2bfa02ec
Joe Stubbs <joestubbs5@gmail.com>
2024-11-26 17:58:12 -0600

    first euclid proof complete.

--- SpecialNumbers/Euclidian.lean
+++ SpecialNumbers/Euclidian.lean
@@ -20,13 +20,27 @@ theorem euclid_two: euclid 2 = 3 := by rfl
 @[simp]
 theorem euclid_three: euclid 3 = 7 := by rfl
 
+lemma factor (n : ℕ) : n^2 - n = (n-1)*n := by
+  rw [Nat.pow_two]
+  exact Eq.symm (Nat.sub_one_mul n n)
+
+
 theorem euclid_eq_prod_euclid (n: ℕ) (h: n≥ 1) :
     euclid (n+1) = ∏ x∈ Finset.Icc 1 n, euclid x + 1 := by
   induction' n with n ih
   contradiction
-  ring_nf
   by_cases c: n = 0
   rw[c]
   simp
+  rw[euclid]
+  simp
+  rw[factor]
   rw[Finset.prod_Icc_succ_top]
-  rw[Finset.empty_product]
+  apply mul_eq_mul_right_iff.mpr
+  apply Or.inl
+  rw[ih]
+  simp
+  omega
+  omega
+  intro
+  linarith
