7ee89451e56772c4c29d303152657be0012be592
Joe Stubbs <joestubbs5@gmail.com>
2024-12-01 13:20:42 -0600

    first proof of euclid_rel_prime

--- SpecialNumbers/Euclidian.lean
+++ SpecialNumbers/Euclidian.lean
@@ -6,6 +6,10 @@ def euclid : ℕ -> ℕ
   | 1 => 2
   | n+1 => (euclid n)^2 - (euclid n) + 1
 
+#eval euclid 5
+#eval euclid 6
+#eval 2*3*7*43*1807 + 1
+
 
 -- The definition conforms to the standard one for the first few examples
 @[simp]
@@ -44,3 +48,73 @@ theorem euclid_eq_prod_euclid (n: ℕ) (h: n≥ 1) :
   omega
   intro
   linarith
+
+theorem euclid_n_geq_one (n: ℕ) (h: n ≥ 1) : euclid n > 1 := by
+  induction' n with n ih
+  contradiction
+  by_cases c: n+1 = 1
+  rw[c]
+  simp
+  have x: n ≥ 1 := by omega
+  rw[euclid]
+  simp
+  have y: euclid n > 1 := ih x
+  rw[pow_two]
+  simp
+  omega
+  intro
+  linarith
+
+
+#eval 1%1
+#eval 2%1
+#eval 2%0
+#eval 1%1
+
+
+theorem euclid_m_n_mod_1 (m n : ℕ) (h1: m < n) (h2: m > 0) :
+    euclid n % euclid m = 1 := by
+  by_cases c: n=0
+  omega
+  have c2: n ≥ 1 := by omega
+  have h1: n = n -1 + 1 := by omega
+  rw [h1]
+  rw[euclid_eq_prod_euclid]
+  have : (euclid m) ∣  ∏ x ∈ Finset.Icc 1 (n-1), euclid x := by
+    apply Finset.dvd_prod_of_mem
+    refine Finset.mem_Icc.mpr ?ha.a
+    omega
+
+  rw[Nat.add_mod]
+  have z : (∏ x ∈ Finset.Icc 1 (n-1), euclid x) % euclid m = 0 := by
+    exact Nat.dvd_iff_mod_eq_zero.mp this
+  rw[z]
+  simp
+  have h3: euclid m > 1 := by
+    apply euclid_n_geq_one
+    omega
+  case neg.h =>
+    linarith
+  apply Nat.mod_eq_of_lt h3
+
+
+theorem gcd_a_b_mod_b_a (a b: ℕ ) : gcd a b = gcd (b % a) a := by
+  apply Nat.gcd_rec
+
+theorem euclid_rel_prime (m n : ℕ) (h: m < n) :
+    gcd (euclid m) (euclid n) = 1 := by
+  by_cases c: m = 0
+  rw[c]
+  simp
+  have h3: m ≥ 1 := by omega
+  rw[gcd_a_b_mod_b_a]
+  rw[euclid_m_n_mod_1]
+  have h4: euclid m > 1 := by
+    apply euclid_n_geq_one
+    linarith
+  apply Nat.gcd_one_left
+  assumption
+  linarith
+
+
+#help tactic
