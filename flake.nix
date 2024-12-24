57be1fb4d3f3dcae4b81ab51f2fca379f1b5a89d
Walter Moreira <walter@waltermoreira.net>
2024-12-23 21:04:18 -0600

    Add blueprints to flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     shell-utils.url = "github:waltermoreira/shell-utils";
   };
@@ -8,6 +8,33 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
       shell = shell-utils.myShell.${system};
+      blueprints = pkgs.python311.pkgs.buildPythonPackage {
+        name = "blueprints";
+        src = pkgs.fetchFromGitHub {
+          repo = "leanblueprint";
+          owner = "PatrickMassot";
+          rev = "v0.0.16";
+          sha256 = "sha256-9hZeFZ94lNVhioVHdgFIyto/9s247nrOAzvUOdABQmU=";
+        };
+        build-system = [
+          pkgs.python311Packages.setuptools
+        ];
+
+        dependencies = with pkgs.python311Packages; [
+            plasTeX
+            plastexshowmore
+            plastexdepgraph
+            click
+            rich
+            rich-click
+            tomlkit
+            jinja2
+            gitpython
+        ];
+
+        pythonImportsCheck = [ "leanblueprint" ];
+      };
+      python = pkgs.python311.withPackages (ps: [ blueprints ]);
     in
     {
       devShell = shell {
@@ -28,6 +55,7 @@
           python311
           bibtool
           findutils
+          python
         ];
       };
     }
