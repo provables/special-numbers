6fa87d0fc49f8f4706ff15291e64d73c54418f2a
Walter Moreira <walter@waltermoreira.net>
2024-12-27 14:59:55 -0600

    More style fixes

--- SpecialNumbers/Eulerian.lean
+++ SpecialNumbers/Eulerian.lean
@@ -18,7 +18,7 @@ of $\{1,2,\ldots,n\}$ with $k$ ascents.
 * [Concrete Mathematics][knuth1989concrete]
 -/
 
-def eulerian (n k:ℕ) : ℕ :=
+def eulerian (n k : ℕ) : ℕ :=
   match n, k with
     | _, 0 => 1
     | 0, _ => 0
@@ -31,12 +31,12 @@ theorem eulerian_of_n_zero (n:ℕ) : eulerian n 0 = 1 := by
 
 theorem eulerian_of_zero : eulerian 0 0 = 1 := eulerian_of_n_zero 0
 
-theorem eulerian_of_zero_k (k:ℕ) (h:k>0): eulerian 0 k = 0 := by
+theorem eulerian_of_zero_k (k : ℕ) (h : k>0): eulerian 0 k = 0 := by
   by_cases c : k = 0
   linarith
   simp [eulerian]
 
-theorem eulerian_of_n_succ_n (n:ℕ) (h:n>0) : ∀ k, k>=n -> eulerian n k = 0 := by
+theorem eulerian_of_n_succ_n (n : ℕ) (h : n>0) : ∀ k, k>=n -> eulerian n k = 0 := by
   induction n with
     | zero => contradiction
     | succ n ih =>
@@ -53,23 +53,23 @@ theorem eulerian_of_n_succ_n (n:ℕ) (h:n>0) : ∀ k, k>=n -> eulerian n k = 0 :
           · simp [ih (by omega) k (by omega), ih (by omega) (k-1) (by omega)]
         · omega
 
-theorem eulerian_of_succ_n_n (n:ℕ) : eulerian (n+1) n = 1 := by
+theorem eulerian_of_succ_n_n (n : ℕ) : eulerian (n+1) n = 1 := by
   induction n with
   | zero => rfl
   | succ n ih =>
-      rw [eulerian]; simp
-      rw [ih]; simp
+      rw [eulerian]
+      simp [ih]
       apply eulerian_of_n_succ_n
       repeat omega
 
-lemma bar [NonAssocRing S] (r s : S) (a : ℕ) : (a • r) * s = r * (a • s):= by
+lemma bar [NonAssocRing S] (r s : S) (a : ℕ) : (a • r) * s = r * (a • s) := by
   rw [mul_smul_comm]
   exact smul_mul_assoc a r s
 
 variable [NonAssocRing R] [Pow R ℕ] [BinomialRing R] [NatPowAssoc R]
 
-lemma foo (r : R) (n : ℕ)
-  : (n+1) • choose (r+1) (n+1) = (r+1) * choose r n := by
+lemma foo (r : R) (n : ℕ) :
+    (n+1) • choose (r+1) (n+1) = (r+1) * choose r n := by
   suffices h : (n+1).factorial • choose (r+1) (n+1) = n.factorial • (r+1) * choose r n by
     rw [Nat.factorial] at h
     nth_rw 1 [Nat.mul_comm] at h
