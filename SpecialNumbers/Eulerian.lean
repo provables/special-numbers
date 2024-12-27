52891bee0338c42a372da58f0df123457f45057f
Walter Moreira <walter@waltermoreira.net>
2024-12-27 15:12:04 -0600

    Styling the space around operators

--- SpecialNumbers/Eulerian.lean
+++ SpecialNumbers/Eulerian.lean
@@ -22,21 +22,24 @@ def eulerian (n k : ℕ) : ℕ :=
   match n, k with
     | _, 0 => 1
     | 0, _ => 0
-    | n+1, k => (k+1)*eulerian n k + (n+1-k)*eulerian n (k-1)
+    | n + 1, k => (k + 1) * eulerian n k + (n + 1 - k) * eulerian n (k - 1)
 
 theorem eulerian_0_0 : eulerian 0 0 = 1 := by rfl
 
-theorem eulerian_of_n_zero (n:ℕ) : eulerian n 0 = 1 := by
+theorem eulerian_of_n_zero (n : ℕ) : eulerian n 0 = 1 := by
   simp [eulerian]
 
 theorem eulerian_of_zero : eulerian 0 0 = 1 := eulerian_of_n_zero 0
 
-theorem eulerian_of_zero_k (k : ℕ) (h : k>0): eulerian 0 k = 0 := by
+theorem eulerian_of_zero_k (k : ℕ) (h : k > 0): eulerian 0 k = 0 := by
   by_cases c : k = 0
   linarith
   simp [eulerian]
 
-theorem eulerian_of_n_succ_n (n : ℕ) (h : n>0) : ∀ k, k>=n -> eulerian n k = 0 := by
+example (n : ℕ) (h : n > 0) (k : ℕ) (hp : k ≥ n): eulerian n k = 0 := by
+  sorry
+
+theorem eulerian_of_n_succ_n (n : ℕ) (h : n > 0) : ∀ k, k ≥ n -> eulerian n k = 0 := by
   induction n with
     | zero => contradiction
     | succ n ih =>
@@ -47,13 +50,13 @@ theorem eulerian_of_n_succ_n (n : ℕ) (h : n>0) : ∀ k, k>=n -> eulerian n k =
             simp
             constructor
             · exact eulerian_of_zero_k k (by linarith)
-            · by_cases d : 1-k = 0
+            · by_cases d : 1 - k = 0
               · exact Or.inl d
-              · exact Or.inr <| eulerian_of_zero_k (k-1) (by omega)
-          · simp [ih (by omega) k (by omega), ih (by omega) (k-1) (by omega)]
+              · exact Or.inr <| eulerian_of_zero_k (k - 1) (by omega)
+          · simp [ih (by omega) k (by omega), ih (by omega) (k - 1) (by omega)]
         · omega
 
-theorem eulerian_of_succ_n_n (n : ℕ) : eulerian (n+1) n = 1 := by
+theorem eulerian_of_succ_n_n (n : ℕ) : eulerian (n + 1) n = 1 := by
   induction n with
   | zero => rfl
   | succ n ih =>
@@ -69,8 +72,8 @@ lemma bar [NonAssocRing S] (r s : S) (a : ℕ) : (a • r) * s = r * (a • s) :
 variable [NonAssocRing R] [Pow R ℕ] [BinomialRing R] [NatPowAssoc R]
 
 lemma foo (r : R) (n : ℕ) :
-    (n+1) • choose (r+1) (n+1) = (r+1) * choose r n := by
-  suffices h : (n+1).factorial • choose (r+1) (n+1) = n.factorial • (r+1) * choose r n by
+    (n + 1) • choose (r + 1) (n + 1) = (r + 1) * choose r n := by
+  suffices h : (n + 1).factorial • choose (r + 1) (n + 1) = n.factorial • (r + 1) * choose r n by
     rw [Nat.factorial] at h
     nth_rw 1 [Nat.mul_comm] at h
     rw [mul_smul] at h
